01d2619255bc71878c44f8945e8f463d9afae445
Jasur <165104527+jasurgraduate@users.noreply.github.com>
2024-03-31 16:14:01 +0500

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -67,23 +67,4 @@ let interval;
             resetTimer();
         });
 
-        // Function to update word count
-function updateWordCount() {
-    var text = document.getElementById("latinTextarea").value;
-    var wordCount = text.trim().split(/\s+/).length;
-    document.getElementById("wordCount").textContent = "Word Count: " + wordCount;
-}
-
-// Call the function on input event
-document.getElementById("latinTextarea").addEventListener("input", updateWordCount);
-
-
- // Function to update word count
- function updateWordCount() {
-    var text = document.getElementById("latinTextarea2").value;
-    var wordCount = text.trim().split(/\s+/).length;
-    document.getElementById("wordCount2").textContent = "Word Count: " + wordCount;
-}
-
-// Call the function on input event
-document.getElementById("latinTextarea2").addEventListener("input", updateWordCount);
+       
